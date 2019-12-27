dd8fefeb7f3bafd3b37d710c228b2de865579a48
Darius Matthews <Dariuscmatthews@gmail.com>
2019-12-26 23:27:44 -0600

    starting main section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navigation/Navbar';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="container">
       <Navbar />
       <Header />
+      <Main />
     </div>
   );
 }

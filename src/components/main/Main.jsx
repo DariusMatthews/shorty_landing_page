333ff17217d1b539bca98952c576e16f53f3b3c5
Darius Matthews <Dariuscmatthews@gmail.com>
2019-12-31 13:50:39 -0600

    finished main section

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,25 +9,31 @@ import CustomizePic from '../../images/icon-fully-customizable.svg'
 export default function Main() {
   return (
     <div className="main">
+
+      {/* main url card */}
       <div>
         <UrlCard />
       </div>
+
+      {/* main text content */}
       <div className="main__text">
         <h1 className="main__header">Advanced Statistics</h1>
         <p className="main__content">Track how your links are performing across the web with our advanced statistics dashboard.</p>
       </div>
+
+      {/* main content cards w/ line connector */}
       <ContentCard
         imgSrc={BrandPic}
         cardTitle={CardContent[0].title}
         cardText={CardContent[0].text}
       />
-      {/* <hr /> */}
+      <div className="main__bar" />
       <ContentCard
         imgSrc={DetailPic}
         cardTitle={CardContent[1].title}
         cardText={CardContent[1].text}
       />
-      {/* <hr /> */}
+      <div className="main__bar" />
       <ContentCard
         imgSrc={CustomizePic}
         cardTitle={CardContent[2].title}
